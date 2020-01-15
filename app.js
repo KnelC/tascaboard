9b7df22aea37721abe0e8afaefeaf7c0a3fa338b
nunofperalta <nunofperalta@gmail.com>
2020-01-15 00:23:50 +0000

    changing port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const port = 80
+const port = 443
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<html><h1>Hello World</h1></html>');
 });
 
 server.listen(port,() => {
